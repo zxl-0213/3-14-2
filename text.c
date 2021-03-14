1d94e21f882f5d5c3bac911e051a1357e0ec3c02
zxl-0213 <287907757@qq.com>
2021-03-14 21:32:49 +0800

    二叉树习题

---
+++ text.c
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+
+bool isUnivalTree(struct TreeNode* root){
+    if(root==NULL)
+    {
+        return true;
+    }
+    if(root->left&&root->val!=root->left->val)
+    {
+        return false;
+    }
+    if(root->right&&root->val!=root->right->val)
+    {
+        return false;
+    }
+
+    return isUnivalTree(root->left)&&isUnivalTree(root->right);
+}
